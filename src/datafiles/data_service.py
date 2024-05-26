49132c994ca6e02ce5752023f2a165136890fa61
Lane Wilson <lane.m.wilson00@gmail.com>
2024-05-26 20:01:21 +0200

    refactored regular mode to use data service to get sentences.

--- src/datafiles/data_service.py
+++ src/datafiles/data_service.py
@@ -3,3 +3,6 @@ import datafiles.dbmanager as dbmanager
 
 def getSoundFile(sentenceId : int) -> str:
     return dbmanager.getSentenceSound(sentenceId)[1]
+
+def getRandomSentencesFromDb(numberToGet : int) ->list:
+    return dbmanager.getSentences(numberToGet)
